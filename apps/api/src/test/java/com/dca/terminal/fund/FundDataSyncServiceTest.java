6dd09dc7b76721ae8ac59432a1d1d817f23c87a9
songshiyu6 <164983832+songshiyu6@users.noreply.github.com>
2026-09-08 17:33:26 +0800

    Keep fund sync service test in transaction

--- apps/api/src/test/java/com/dca/terminal/fund/FundDataSyncServiceTest.java
+++ apps/api/src/test/java/com/dca/terminal/fund/FundDataSyncServiceTest.java
@@ -15,12 +15,14 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
+import org.springframework.transaction.annotation.Transactional;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 @SpringBootTest
 @ActiveProfiles("test")
+@Transactional
 class FundDataSyncServiceTest {
     @Autowired InstrumentRepository instrumentRepository;
     @Autowired FundProfileRepository profileRepository;
